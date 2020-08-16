8bf22fd6e80dc26f96bb1bf02f273e01febe184f
King Elisha <ek.chibueze@gmail.com>
2020-08-16 20:57:46 +0100

    feat: added apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import "./App.scss";
 
+const apolloClient = new ApolloClient({
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:7000/graphql"
+      : "/graphql",
+  cache: new InMemoryCache(),
+});
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </ApolloProvider>
   );
 }
